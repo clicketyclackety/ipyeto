a86ae7ae8c22ccf240977fbd2e5ea1c321d4d160
Callum Sykes <callumsykes@protonmail.com>
2025-02-17 11:42:14 +0100

    init

---
+++ MainForm.cs
@@ -0,0 +1,13 @@
+using Eto.Forms;
+using Eto.Drawing;
+
+namespace ipyeto;
+
+public partial class MainForm : Form
+{
+	public MainForm()
+	{
+		Title = "My IronPython Form";
+		MinimumSize = new Size(200, 200);
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+
+using Eto.Forms;
+
+using IronPython.Hosting;
+using Microsoft.Scripting.Hosting;
+
+// Notes : https://putridparrot.com/blog/hosting-ironpython-in-a-c-application/
+
+namespace ipyeto;
+
+class Program
+{
+
+	private static ScriptEngine Engine { get; set; }
+	private static ScriptScope Scope { get; set; }
+
+	[STAThread]
+	static void Main(string[] args)
+	{
+		Engine = Python.CreateEngine();
+		Scope = Engine.CreateScope();
+
+		Engine.Execute(@"
+import clr
+clr.AddReference('Eto')
+# clr.AddReference('Eto.Wpf') # May or may not be necessary
+		", Scope);
+
+		var app = new Application(Eto.Platform.Detect);
+		var form = new MainForm();
+
+		form.PreLoad += RunPython;
+		
+		Scope.SetVariable("__form__", form);
+		
+		app.Run(form);
+	}
+
+	private static void RunPython(object sender, EventArgs e)
+	{
+		Engine.Execute(@$"
+from Eto.Forms import Button
+
+control = Button();
+control.Text = 'hello!'
+__form__.Content = control
+			", Scope);	
+	}
+
+}
